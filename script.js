5c6dbee6e883246ec1b502b1a0ce89e1095c49e1
Robert Kitowski <robertokitowski@gmail.com>
2023-02-18 14:34:25 +0100

    Added removed function

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 const inputName = document.getElementById("name");
 const btnAdd = document.getElementById("add");
 const allPlayers = document.getElementById("all-players");
-const profileIcon = `<i class="fa-solid fa-user"></i>`;
+const result = document.getElementById("result");
 
-// console.log(inputName.value);
+let number = 0;
+
+let players = [];
 
 btnAdd.addEventListener("click", addPlayer);
 
 function addPlayer(e) {
   e.preventDefault();
-  console.log(inputName.value);
   let playerName = inputName.value;
-  const span = document.createElement("span");
-  span.innerHTML = `${profileIcon}  ${playerName}`;
-  allPlayers.appendChild(span);
-  inputName.value = "";
+  if (playerName) {
+    number++;
+
+    const span = document.createElement("span");
+
+    span.innerHTML = ` <div class="player">
+    <i class="fa-solid fa-user"></i>
+    <span>${playerName}</span>
+    <button class="x"> 
+      X
+    </button>
+    </div>`;
+
+    players.push(playerName);
+    allPlayers.appendChild(span);
+    inputName.value = "";
+    numberResult();
+  }
+
+  remove();
+}
+
+function remove() {
+  const allRemovePlayer = document.querySelectorAll(".x");
+  allRemovePlayer.forEach((x) => {
+    x.addEventListener("click", () => {
+      x.parentElement.remove();
+      numberResult();
+    });
+  });
+}
+
+function numberResult() {
+  const listItem = document.getElementsByClassName("player");
+  result.innerText = listItem.length.toString();
 }
